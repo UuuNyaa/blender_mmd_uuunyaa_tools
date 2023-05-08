e9d24b57b679ce3e8f37fb3d1f8627de846cabe6
UuuNyaa <uuunyaa@gmail.com>
2023-05-08 03:22:45 -0700

    Release preparation

--- mmd_uuunyaa_tools/__init__.py
+++ mmd_uuunyaa_tools/__init__.py
@@ -27,7 +27,7 @@ bl_info = {
     'name': 'mmd_uuunyaa_tools',
     'description': 'Utility tools for MMD model & scene editing by Uuu(/>ω<)/Nyaa!.',
     'author': 'UuuNyaa',
-    'version': (1, 7, 1),
+    'version': (1, 7, 2),
     'blender': (2, 83, 0),
     'warning': '',
     'location': 'View3D > Sidebar > MMD Tools Panel',
